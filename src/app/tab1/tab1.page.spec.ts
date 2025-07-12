e2f263f525e1d78c58fde077e8ed7d140c0f60f3
iredavid <ireoluwadavid44@gmail.com>
2025-07-12 03:41:32 +0100

    Save current changes before Angular update

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,10 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Tab1Page } from './tab1.page';
-<<<<<<< HEAD
-=======
 import { DataService } from '../services/data.service';
->>>>>>> 8bdfca4c816053bf7872ae5feaa23a42cc458c84
 
 describe('Tab1Page', () => {
   let component: Tab1Page;
@@ -15,12 +12,8 @@ describe('Tab1Page', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-<<<<<<< HEAD
-
-=======
   providers: [DataService];
  
->>>>>>> 8bdfca4c816053bf7872ae5feaa23a42cc458c84
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,37 +1,13 @@
-<<<<<<< HEAD
-import { Component, inject, OnInit} from '@angular/core';
-import { IonHeader,IonMenu,IonButton,IonMenuToggle,  IonCardHeader, IonCardTitle, IonCardSubtitle, IonToolbar, IonCardContent,IonCard, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { DataService } from '../services/data.service';
-=======
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {IonHeader,IonGrid,IonCol, IonRow, IonToolbar,IonSegment,IonSegmentButton,IonSelectOption,IonList,IonSelect, IonIcon,IonSearchbar,IonTitle, IonNote,IonContent,IonItem,IonLabel,IonAvatar,IonBadge,  } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../environments/explore-container/explore-container.component';
 import{register} from 'swiper/element/bundle'
 register()
->>>>>>> 8bdfca4c816053bf7872ae5feaa23a42cc458c84
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-<<<<<<< HEAD
-  imports: [IonHeader,IonMenu,IonButton,IonMenuToggle,  IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonToolbar, IonCard, IonTitle, IonContent, ExploreContainerComponent]
-})
-export class Tab2Page implements OnInit{
-footballClub:any 
-dataService= inject(DataService)
-
-  constructor() {
-    this.dataService.getFot()
-  }
-
-  ngOnInit(){
-     console.log(this.footballClub);
-       this.footballClub = this.dataService.footballClub
-  }
-goToDetails(){}
-=======
     schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports: [IonHeader,IonGrid,IonCol, IonRow, IonToolbar,IonSegment,IonSegmentButton,IonSelectOption,IonList,IonSelect, IonIcon,IonSearchbar,IonTitle, IonNote,IonContent,IonItem,IonLabel,IonAvatar,IonBadge,]
 })
@@ -39,5 +15,4 @@ export class Tab2Page {
 
   constructor() {}
 
->>>>>>> 8bdfca4c816053bf7872ae5feaa23a42cc458c84
 }
